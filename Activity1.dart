94b0f3eb7359a819c51c159aedffab46aa91c4b2
davidsuee <badillodavidandrei@gmail.com>
2025-08-08 17:43:55 +0800

    Understanding the Dart

--- Activity1.dart
+++ Activity1.dart
@@ -1,6 +1,13 @@
 void main() {
-  String greeting = "Welcome";
-  String user = "Alice"; 
 
-  print("$greeting, $user!"); 
+  String greeting = "Welcome";
+  String user = "Alice";
+
+  print("$greeting, $user!");
+
+
+  dynamic userDynamic = 25;
+
+
+  print("$greeting, $userDynamic!");
 }
--- Activity2.dart
+++ Activity2.dart
@@ -1,7 +1,23 @@
 void main() {
-  dynamic user = "Alice";  
-  print("Welcome, $user!");  
 
-  user = 25;  
-  print("Welcome, $user!");  
+  String rectanglePerimeter = calculatePerimeter(10, 5, false);
+  print(rectanglePerimeter);
+
+
+  String squarePerimeter = calculatePerimeter(7, 7, true);
+  print(squarePerimeter);
+}
+
+String calculatePerimeter(double width, double height, [bool isSquare = false]) {
+  double perimeter;
+
+  if (isSquare) {
+    perimeter = 4 * width;
+  } else {
+    perimeter = 2 * (width + height);
+  }
+
+  return isSquare
+      ? "The perimeter of the square is: $perimeter"
+      : "The perimeter of the rectangle is: $perimeter";
 }
